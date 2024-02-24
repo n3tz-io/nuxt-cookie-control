cf63dc7cbf3a56eecbf8cfe8700fb91a8a20e85f
fujin <tarik@n3tz.io>
2024-02-24 14:50:31 +0100

    feat: change to declineAll to decline consent

--- src/runtime/components/CookieControl.vue
+++ src/runtime/components/CookieControl.vue
@@ -14,7 +14,9 @@
                   class="flex justify-between"
                 >
                   <h2 v-text="localeStrings?.bannerTitle" />
-                  <span class="close cursor-pointer" @click="decline()">x</span>
+                  <span class="close cursor-pointer" @click="declineAll()"
+                    >x</span
+                  >
                 </div>
                 <h2 v-else v-text="localeStrings?.bannerTitle" />
                 <p v-text="localeStrings?.bannerDescription" />
